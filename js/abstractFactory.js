3b722842744659047d2785dbac2575555a6af19b
Carlos <kaijinw@moguyun.com>
2022-12-14 17:39:03 +0800

    add abstract factory pattern

---
+++ js/abstractFactory.js
@@ -0,0 +1,73 @@
+"use strict";
+/**
+ * 抽象工厂模式 (Abstract Factory)
+ * 顾名思义，抽象工厂模式并不直接产生实例，而是用于对产品类簇的创建
+ * TypeScript 中有所谓的抽象类，是其一定的应用
+ * JavaScript 中会狭义一些，更多代表的生成工厂的方法
+ */
+/**
+ *  TypeScript 示例场景
+ * 几何体不能直接实例化，并包含计算面积的抽象方法
+ * 圆形，正方形，正三角形继承自抽象类几何体，自行实现抽象方法
+ */
+class Geometry {
+    echoArea() {
+        console.log(Object.getPrototypeOf.call(this, this).constructor.name, this.getArea());
+    }
+}
+class Circle extends Geometry {
+    constructor(r) {
+        super();
+        this.r = r;
+    }
+    getArea() {
+        return Math.PI * Math.pow(this.r, 2);
+    }
+}
+class Square extends Geometry {
+    constructor(w) {
+        super();
+        this.w = w;
+    }
+    getArea() {
+        return Math.pow(this.w, 2);
+    }
+}
+class EquilateralTriangle extends Geometry {
+    constructor(w) {
+        super();
+        this.h = w;
+    }
+    getArea() {
+        return Math.pow(this.h, 2) / 2;
+    }
+}
+const circle = new Circle(2);
+const square = new Square(4);
+const equilateralTriangle = new EquilateralTriangle(4);
+circle.echoArea();
+square.echoArea();
+equilateralTriangle.echoArea();
+// echo separator
+console.log(Array.from(new Array(30)).join('='));
+function getGeometryFactory(type) {
+    switch (type) {
+        case 'circle':
+            return Circle;
+        case 'square':
+            return Square;
+        case 'equilateralTriangle':
+            return EquilateralTriangle;
+        default:
+            throw new Error('not collect type');
+    }
+}
+const Geom1 = getGeometryFactory('circle');
+const Geom2 = getGeometryFactory('square');
+const Geom3 = getGeometryFactory('equilateralTriangle');
+const circle2 = new Geom1(1);
+const square2 = new Geom2(1);
+const equilateralTriangle2 = new Geom2(1);
+circle2.echoArea();
+square2.echoArea();
+equilateralTriangle2.echoArea();
